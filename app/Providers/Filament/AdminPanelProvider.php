8227607bd9df1f6e60b6ba2461f65fad144317c3
rawrmingos-design <rawrmingos@gmail.com>
2025-10-04 19:19:24 +0700

    fix: change Filament path from /admin to /filament to avoid route conflict

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,8 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('filament')
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
