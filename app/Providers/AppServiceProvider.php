27698e1e2709dac57449cc6030f02bd68c858b1b
rawrmingos-design <rawrmingos@gmail.com>
2025-10-05 10:40:47 +0700

    fix: resolve 'array offset on null' error in AppServiceProvider and DashboardStatsWidget

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,43 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $config = \DB::table('setting_webs')->where('id',1)->first();
-        
-        View::share('config',$config);
-        
+        try {
+            $config = \DB::table('setting_webs')->where('id',1)->first();
+            
+            // Provide default values if config is null
+            if (!$config) {
+                $config = (object) [
+                    'judul_web' => 'Game Top-Up',
+                    'deskripsi_web' => 'Platform Top-Up Game Terpercaya',
+                    'keywords' => 'top up, game, diamond, voucher',
+                    'logo_favicon' => '/assets/logo/favicon.ico',
+                    'warna1' => '#222222',
+                    'warna2' => '#d06800', 
+                    'warna3' => '#ffa54a',
+                    'warna4' => '#ff8040',
+                    'wa_key' => '',
+                    'nomor_admin' => ''
+                ];
+            }
+            
+            View::share('config', $config);
+            
+        } catch (\Exception $e) {
+            // Fallback config if database is not available
+            $config = (object) [
+                'judul_web' => 'Game Top-Up',
+                'deskripsi_web' => 'Platform Top-Up Game Terpercaya',
+                'keywords' => 'top up, game, diamond, voucher',
+                'logo_favicon' => '/assets/logo/favicon.ico',
+                'warna1' => '#222222',
+                'warna2' => '#d06800', 
+                'warna3' => '#ffa54a',
+                'warna4' => '#ff8040',
+                'wa_key' => '',
+                'nomor_admin' => ''
+            ];
+            
+            View::share('config', $config);
+        }
     }
 }
