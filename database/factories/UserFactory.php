6a4bba9fc65472b774b621b41c72f94594e26043
rawrmingos-design <rawrmingos@gmail.com>
2026-06-14 01:01:39 +0700

    test(factories): enhance user factory for reseller testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,15 +8,17 @@ class UserFactory extends Factory
 {
     public function definition()
     {
+        $id = uniqid();
         return [
-            'name'          => $this->faker->name(),
-            'username'      => $this->faker->unique()->userName(),
-            'email'         => $this->faker->unique()->safeEmail(),
+            'name'          => 'Test User ' . $id,
+            'username'      => 'user_' . $id,
+            'email'         => 'test_' . $id . '@example.com',
             'password'      => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'role'          => 'Member',
             'balance'       => 0,
             'point_balance' => 0,
-            'no_wa'         => '08' . $this->faker->numerify('##########'),
+            'no_wa'         => '0812' . rand(10000000, 99999999),
+            'created_at'    => now()->subDays(7), // Ensure account meets 7-day minimum age requirement
         ];
     }
 }
