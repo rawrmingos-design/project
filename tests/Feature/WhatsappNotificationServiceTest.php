c4b0f1ad74f780d5d6f50e53fcbdf639e0d88840
rawrmingos-design <rawrmingos@gmail.com>
2026-08-19 14:39:26 +0700

    debug(test): var_dump openwa result (temp)

--- tests/Feature/WhatsappNotificationServiceTest.php
+++ tests/Feature/WhatsappNotificationServiceTest.php
@@ -515,7 +515,7 @@ class WhatsappNotificationServiceTest extends TestCase
         $result = app(WhatsappNotificationService::class)
             ->sendTestMessage('[REDACTED]', 'Halo dari OpenWA');
 
-        fwrite(STDERR, 'DEBUG openwa provider result: ' . json_encode($result) . PHP_EOL);
+        var_dump($result);
 
         $this->assertTrue($result['success']);
         $this->assertSame('openwa', $result['provider']);
