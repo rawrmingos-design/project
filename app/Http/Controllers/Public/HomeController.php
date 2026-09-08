5b1c8bc25c86a2d9c123a4fac88c8cad90536626
rawrmingos-design <rawrmingos@gmail.com>
2026-09-08 10:18:04 +0700

    feat: centralize inertia seo metadata contract

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Public;
 
-use App\Helpers\HtmlSanitizer;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\IndexController as LegacyHomeController;
 use App\Services\PublicHomePageDataService;
 use App\Services\PublicSiteConfigService;
+use App\Services\SeoMetadataService;
 use App\Support\PublicThemeRegistry;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,6 +16,7 @@ class HomeController extends Controller
     public function __invoke(
         PublicHomePageDataService $homePageDataService,
         PublicSiteConfigService $siteConfigService,
+        SeoMetadataService $seoMetadataService,
         LegacyHomeController $legacyHomeController,
     ): Response|\Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
@@ -28,13 +29,7 @@ class HomeController extends Controller
         $data = $homePageDataService->getData();
 
         return Inertia::render('Public/Home', array_merge($data, [
-            'meta' => [
-                'title' => $settings->judul_web,
-                'description' => HtmlSanitizer::toPlainText($settings->deskripsi_web, 180),
-                'keywords' => $settings->keywords,
-                'canonical' => url('/id'),
-                'image' => url($siteConfigService->normalizeAssetPath($settings->logo_favicon)),
-            ],
+            'meta' => $seoMetadataService->homepage(),
         ]));
     }
 }
